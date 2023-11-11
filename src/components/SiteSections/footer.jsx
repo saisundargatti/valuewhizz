c59d245cc8b7a3b6407ec8d56a2e9c2dcf803b24
saisundargatti <saisundargatti365@gmail.com>
2023-11-12 00:49:28 +0530

    reverted changes

--- src/components/SiteSections/footer.jsx
+++ src/components/SiteSections/footer.jsx
@@ -91,7 +91,21 @@ function Footer() {
             </svg>
           </a>
           <a className="ml-3 text-gray-500">
-            <img src="/my-loan-calc/public/8658461.png" alt="EMI Calculator" />
+            <svg
+              fill="currentColor"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="0"
+              className="w-5 h-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="none"
+                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+              ></path>
+              <circle cx="4" cy="4" r="2" stroke="none"></circle>
+            </svg>
           </a>
         </span>
       </div>
--- src/components/SiteSections/header.jsx
+++ src/components/SiteSections/header.jsx
@@ -6,7 +6,18 @@ function Header() {
     <header className="text-grey-600 body-font h-22 md:h-20 fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex flex-wrap flex-col p-5 md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 ">
-          <img src="/my-loan-calc/public/8658461.png" alt="EMI Calculator" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
           <span className="ml-3 text-xl">ValueWhizz</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
